9e1c9658fa8d319592afd334d23816400c80ae42
William Tsoi <william@williamtsoi.net>
2024-09-24 14:06:53 +0800

    1.8.2 change

--- src/main/java/com/google/BatchJob.java
+++ src/main/java/com/google/BatchJob.java
@@ -18,48 +18,49 @@
 
 package com.google;
 
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.java.ExecutionEnvironment;
 
 /**
- * Skeleton for a Flink DataStream Job.
+ * Skeleton for a Flink Batch Job.
  *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ * <p>For a tutorial how to write a Flink batch application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
  *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ * <p>To package your application into a JAR file for execution,
+ * change the main class in the POM.xml file to this class (simply search for 'mainClass')
+ * and run 'mvn clean package' on the command line.
  */
-public class DataStreamJob {
+public class BatchJob {
 
 	public static void main(String[] args) throws Exception {
-		// Sets up the execution environment, which is the main entry point
-		// to building Flink applications.
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		// set up the batch execution environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
 		/*
 		 * Here, you can start creating your execution plan for Flink.
 		 *
 		 * Start with getting some data from the environment, like
-		 * 	env.fromSequence(1, 10);
+		 * 	env.readTextFile(textPath);
 		 *
-		 * then, transform the resulting DataStream<Long> using operations
+		 * then, transform the resulting DataSet<String> using operations
 		 * like
 		 * 	.filter()
 		 * 	.flatMap()
-		 * 	.window()
-		 * 	.process()
+		 * 	.join()
+		 * 	.coGroup()
 		 *
 		 * and many more.
-		 * Have a look at the programming guide:
+		 * Have a look at the programming guide for the Java API:
 		 *
-		 * https://nightlies.apache.org/flink/flink-docs-stable/
+		 * http://flink.apache.org/docs/latest/apis/batch/index.html
+		 *
+		 * and the examples
+		 *
+		 * http://flink.apache.org/docs/latest/apis/batch/examples.html
 		 *
 		 */
 
-		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
+		// execute program
+		env.execute("Flink Batch Java API Skeleton");
 	}
 }
